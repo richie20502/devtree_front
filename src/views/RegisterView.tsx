afd8af59e36b53ba3f2497385fc3f5a014204183
richie20502 <richie.lugo.recillas.1990@gmail.com>
2025-09-13 21:32:31 -0600

    estilos navegacion

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 export default function RegisterView() {
   return (
     <>
-      <nav>
-        <Link to="/auth/login">¿Ya tienes cuenta? Inicia Sesión</Link>
+      <h1 className="text-4xl text-white font-bold">Crear cuenta</h1>
+      <nav className="mt-10">
+        <Link className="text-center text-white text-lg block" to="/auth/login">
+          ¿Ya tienes cuenta? Inicia Sesión
+        </Link>
       </nav>
     </>
   );
