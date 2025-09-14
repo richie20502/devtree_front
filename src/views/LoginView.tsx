f1a8611639b805883dea34e8a81cc79a246c03b9
richie20502 <richie.lugo.recillas.1990@gmail.com>
2025-09-13 21:17:25 -0600

    Layout principal

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 export default function LoginView() {
   return (
     <>
-      <div>LoginViewkjkljlkjkljlkljkllkj</div>
       <nav>
         <Link to="/auth/register">No Tienes Cuenta? Crea una aquí</Link>
       </nav>
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 export default function RegisterView() {
   return (
     <>
-      <div>RegisterView</div>
       <nav>
         <Link to="/auth/login">¿Ya tienes cuenta? Inicia Sesión</Link>
       </nav>
